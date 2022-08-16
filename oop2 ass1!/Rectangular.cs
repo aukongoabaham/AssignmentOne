5a567817e45388ca99d4ca83d9085898afdea621
Abraham-Aukongo <111201901+aukongoabaham@users.noreply.github.com>
2022-08-16 23:14:15 +0200

    Update Rectangular.cs

--- oop2 ass1!/Rectangular.cs
+++ oop2 ass1!/Rectangular.cs
@@ -10,8 +10,27 @@ namespace oop2_ass1_
     {
         public Rectangular(string colour, double volume, string shape) : base(colour, volume, shape)
         {
-
-
+            string [] recContainer = new recContainer [100];  
+            double [] volumes = new volumes [recContainer.Length];
+            int count = -1;
+            
+             
+             
+             
+             for (int iCount = 0; iCount < volumes.Lenght;iCount++)
+             {
+                  if (volumes[iCount] > iMaxValue)
+                  {
+                    count = iCount;
+                    iMaxValue = volumes[iCount];
+                    console.WriteLine("the highest volume is" + volumes[icount] + count + "& the rectagler shape is" +recContainer[count]);
+                    console.ReadLine();
+                    
+                  }
+             
+             
+             
+             }
 
        
         }
